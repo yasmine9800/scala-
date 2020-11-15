e62c97548eb921c82536ec1739f9b78e8f7561f2
Pierre Boudes <pierre@mindsized.org>
2020-11-15 20:30:25 +0100

    Actor hierarchy from the documentation (augmented)

---
+++ src/main/scala/com/example/ActorHierarchyExperiments.scala
@@ -0,0 +1,51 @@
+package com.example
+
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.AbstractBehavior
+import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.ActorPath
+
+object PrintMyActorRefActor {
+  def apply(): Behavior[String] =
+    Behaviors.setup(context => new PrintMyActorRefActor(context))
+}
+
+class PrintMyActorRefActor(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+
+  override def onMessage(msg: String): Behavior[String] =
+    msg match {
+      case "printit" =>
+        val secondRef = context.spawn(Behaviors.empty[String], "second-actor")
+        println(s"Second: $secondRef")
+        this
+      case "hello" => 
+        println("hello")
+        this  
+    }
+}
+
+object Main {
+  def apply(): Behavior[String] =
+    Behaviors.setup(context => new Main(context))
+
+}
+
+class Main(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+  override def onMessage(msg: String): Behavior[String] =
+    msg match {
+      case "start" =>
+        println(s"Main: ${context.self}")
+        val firstRef = context.spawn(PrintMyActorRefActor(), "first-actor")
+        println(s"First: $firstRef")
+        firstRef ! "printit"
+        this
+    }
+}
+
+object ActorHierarchyExperiments extends App {
+  val testSystem = ActorSystem(Main(), "testSystem")
+  testSystem ! "start"
+  println(s"ActorSystem: $testSystem")
+}
