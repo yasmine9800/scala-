481c1435e2e5e9e39d86d17d9c43f403f21a06c9
Pierre Boudes <pierre@mindsized.org>
2020-11-15 20:31:16 +0100

    StartStop from the documentation

---
+++ src/main/scala/com/example/StartStop.scala
@@ -0,0 +1,77 @@
+package com.example
+
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.AbstractBehavior
+import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.Signal
+import akka.actor.typed.PostStop
+
+object StartStopActor1 {
+  def apply(): Behavior[String] =
+    Behaviors.setup(context => new StartStopActor1(context))
+}
+
+class StartStopActor1(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+  println("first started")
+  context.spawn(StartStopActor2(), "second")
+
+  override def onMessage(msg: String): Behavior[String] =
+    msg match {
+      case "stop" => Behaviors.stopped
+    }
+
+  override def onSignal: PartialFunction[Signal, Behavior[String]] = {
+    case PostStop =>
+      println("first stopped")
+      this
+  }
+
+}
+
+object StartStopActor2 {
+  def apply(): Behavior[String] =
+    Behaviors.setup(new StartStopActor2(_))
+}
+
+class StartStopActor2(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+  println("second started")
+
+  override def onMessage(msg: String): Behavior[String] = {
+    // no messages handled by this actor
+    Behaviors.unhandled
+  }
+
+  override def onSignal: PartialFunction[Signal, Behavior[String]] = {
+    case PostStop =>
+      println("second stopped")
+      this
+  }
+
+}
+
+
+object Main {
+  def apply(): Behavior[String] =
+    Behaviors.setup(context => new Main(context))
+
+}
+
+class Main(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+  override def onMessage(msg: String): Behavior[String] =
+    msg match {
+      case "start" =>
+        val first = context.spawn(StartStopActor1(), "first")
+        first ! "stop"
+        this
+    }
+}
+
+object StartStopExample extends App {
+  val testSystem = ActorSystem(Main(), "testSystem")
+  testSystem ! "start"
+  println(s"ActorSystem: $testSystem")
+}
+
+
